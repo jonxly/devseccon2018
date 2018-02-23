64df5561754ec9a786a0a18a6fa03cae434e466f
jonxly <jubot@github.com>
2018-02-23 14:13:23 +0800

    removed mongo secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://jmongo:jmongo@ds147518.mlab.com:47518/jmongo
+# export MONGODB_CREDENTIALS=mongodb://jmongo:jmongo@ds147518.mlab.com:47518/jmongo
 
 # finally, run the app in dev mode...
 npm run dev
